23a7b9ad53d3ce2787f5642f0bf87debe7cc5e09
ChandraMohan <chandra.nair@ust-global.com>
2018-07-13 19:01:45 +0530

    delete

--- cSyntaxes/src/condition/for-loop1.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-   int i;
-   for (i=1; i<=3; i++)
-   {
-       printf("%d\n", i);
-   }
-   return 0;
-}
--- cSyntaxes/src/condition/if3.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{ 
-    if (;)
-        printf("Inside if: how does the semicolon or any other character"
-               " but digits as a condition in if statement treated? or"
-               " cause COMPILATION ERROR!\n");
-    return 0;
-}
